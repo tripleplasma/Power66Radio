4769e2e5abd2dc38fe7ba3e6b0aa3c89bbb93ffc
Kung Lee <kulee@hbs.net>
2020-12-24 16:35:14 -0600

    Add project files.

---
+++ Modules/InfoModule.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power66Radio
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("tune")]
+        public Task TuneIn()
+        {
+            SocketGuildChannel channel = Context.Guild.GetChannel(Context.Channel.Id);
+            var everyone = channel.Users;
+            var theRole = Context.Guild.Roles.Where(x => x.Position == Context.Guild.Roles.Count-2).FirstOrDefault();
+            return ReplyAsync(theRole.Mention +"\n"
+                +(channel.GetUser(Context.User.Id).Nickname == null ? Context.User.Username : channel.GetUser(Context.User.Id).Nickname) 
+                + " in the buildin!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+namespace Power66Radio
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        public static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+        
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            _client.Log += Log;
+
+            var token = "NzkxNzY1Mjc4NDE3MjIzNjkw.X-T64A.OrA9ykVuHFTc_bRiFT_5WTCCgPc";
+
+            _client.Log += _client_Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandelCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(),_services);
+        }
+
+        private async Task HandelCommandAsync(SocketMessage msg)
+        {
+            var message = msg as SocketUserMessage;
+            SocketCommandContext context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if(message.HasStringPrefix("!",ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.Error);
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        
+    }
+}
